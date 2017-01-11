b5b5f47b8d0d6374bf8f1fe70a89c672d0ceb910
guihubu <nastvogelw@net-up.de>
2017-01-11 20:21:45 +0100

    added bnootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user_account!
 
   def index
-  	render :index
+    render :index
   end
+
+  private
+
+    # Overwriting the sign_out redirect path method
+    def after_sign_out_path_for(resource_or_scope)
+      root_path
+    end
 end
